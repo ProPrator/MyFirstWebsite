e44bbcce3400e826a6dc38e144e60faeb33d406d
Pro.Orator <Pro.Orator@gmail.com>
2020-06-01 11:21:04 +0300

    Make all migrations and model

---
+++ database/migrations/2020_06_01_075024_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('text');
+
+            $table->integer('user_id')->unsigned()->default(1);
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('article_id')->unsigned()->default(1);
+
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
